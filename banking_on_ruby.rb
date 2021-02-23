eaf83decf971cdd844658411552e9ee9e5d379c3
Mihndim <jelsieprisca2016@gmail.com>
2021-02-23 14:08:28 -0500

    Add display balance method

--- banking_on_ruby.rb
+++ banking_on_ruby.rb
@@ -4,6 +4,13 @@ class Account
   @name = name 
   @balance = balance 
   end
+  def display_balance(pin_number)
+  if pin_number == pin
+  puts "Balance: $#{@balance}."
+  else 
+  puts pin_error
+  end
+  end
   private
   def pin
   @pin = 1234
