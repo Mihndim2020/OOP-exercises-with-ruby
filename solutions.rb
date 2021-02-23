426be9be344ecb0d2d1a9bb5179be0f9d93dfdb7
Mihndim <jelsieprisca2016@gmail.com>
2021-02-23 07:08:10 -0500

    Add speed_up, brake and shut_off methods

--- solutions.rb
+++ solutions.rb
@@ -1,7 +1,24 @@
 class MyCar
-    def initialize(year, color, model)
-        @year = year
-        @color = color
-        @model = model
-        end
+  def initialize(year, color, model)
+    @year = year
+    @color = color
+    @model = model
+    @current_speed = 0
+  end
+  def speed_up(number)
+    @current_speed += number
+    puts "You're driving at #{@current_speed}"
+  end
+  def brake(number)
+    @current_speed -= number
+    puts "You're driving at #{@current_speed}"
+  end 
+  def shut_off 
+    @current_speed = 0
+    puts "You're driving at #{@current_speed}"
+  end
+  def current_speed
+    puts "Your current speed is #{@current_speed}"
+  end
+
 end
